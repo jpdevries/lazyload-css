3bac5620ccf719952c02f9a1a105915160cffeda
JP DeVries <mail@devries.jp>
2017-03-26 15:26:04 +0200

    adding more tests

--- tests/lazy.js
+++ tests/lazy.js
@@ -2,7 +2,10 @@ var href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-a
 id = 'font-awesome-4.7.0';
 
 describe("lazyLoadCSS", function () {
-  beforeEach(function () {});
+  beforeEach(function () {
+    const links =  document.head.querySelectorAll('link');
+    for(var i = 0; i < links.length; i++) links[i].remove();
+  });
 
   it("returns a promise", function () {
     expect(lazyLoadCSS(href, id)).toEqual(jasmine.any(Promise));
@@ -26,4 +29,54 @@ describe("lazyLoadCSS", function () {
       });
     });
   });
+
+  it("should add a media attribute", function(done) {
+    lazyLoadCSS(href, id).then(function (sheet) {
+      expect(document.getElementById(id).getAttribute('media')).toBe('all');
+      done();
+    }).catch(function (err) {
+      fail(err);
+      done();
+    });
+  });
+
+  it("should add a configurable media attribute", function(done) {
+    const id = 'media';
+    if(document.getElementById(id)) document.getElementById(id).remove();
+
+    lazyLoadCSS(href, id, 'screen').then(function (sheet) {
+      expect(document.getElementById(id).getAttribute('media')).toBe('screen');
+      if(document.getElementById(id)) document.getElementById(id).remove();
+      done();
+    }).catch(function (err) {
+      fail(err);
+      done();
+    });
+  });
+
+  it("should add a configurable rel attribute", function(done) {
+    const id = 'rel';
+
+    lazyLoadCSS(href, id, 'all', 'stylesheet').then(function (sheet) {
+      expect(document.getElementById(id).getAttribute('rel')).toBe('stylesheet');
+      done();
+    }).catch(function (err) {
+      fail(err);
+      done();
+    });
+  });
+
+  it("should add a configurable type attribute", function(done) {
+    const id = 'type';
+
+    lazyLoadCSS(href, id, undefined, undefined, 'text/css').then(function (sheet) {
+      expect(document.getElementById(id).getAttribute('type')).toBe('text/css');
+      done();
+    }).catch(function (err) {
+      fail(err);
+      done();
+    });
+  });
+
+
 });
